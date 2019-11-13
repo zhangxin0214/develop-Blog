3070f5e3e53c736c84f9d56a3d052c424cff741f
xrswx100 <zhangxin0214@100tal.com>
2019-11-13 15:04:09 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,24 @@
 # 构建
+yarn install
 yarn build
 
+# 推送源代码
+git add -A
+git commit -m 'update'
+git config --global user.name  "zhangxin0214"
+git config --global user.email  "zhangxin910214@163.com"
+git remote add origin git@github.com:zhangxin0214/develop-Blog.git
 # 导航到构建输出目录
 cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
-git config --global user.name  "zhangxin0214"
-git config --global user.email  "zhangxin910214@163.com"
-git remote add origin git@github.com:zhangxin0214/docs.git
+git config --global user.name  "xrswx100"
+git config --global user.email  "zhangxin0214@100tal.com"
+git remote add origin git@github.com:xrswx100/xes-template-docs.git
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
-git push origin master
+git push -f origin master
 
 cd -
