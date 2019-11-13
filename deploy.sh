9967db2f63ed97fae8d35a512a84d670b081ef4d
xrswx100 <zhangxin0214@100tal.com>
2019-11-13 16:26:25 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # 推送源代码
 git add -A
 git commit -m 'update'
-# git config --global user.name  "zhangxin0214"
-# git config --global user.email  "zhangxin910214@163.com"
+git config --global user.name  "zhangxin0214"
+git config --global user.email  "zhangxin910214@163.com"
 git remote add origin git@github.com:zhangxin0214/develop-Blog.git
 git push -f origin master
 # 构建
@@ -16,9 +16,9 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git config --global user.name  "xrswx100"
-git config --global user.email  "zhangxin0214@100tal.com"
-git config --global user.password  "aixin561521"
+# git config --global user.name  "xrswx100"
+# git config --global user.email  "zhangxin0214@100tal.com"
+# git config --global user.password  "aixin561521"
 git remote add origin git@github.com:xrswx100/xes-template-docs.git
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
